7b8cb62f44f5b3727c5677c5963df4bb68ebf025
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-07 18:41:32 -0800

    Fix formatting issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,36 @@
-import { GeistSans } from 'geist/font/sans'
-import './globals.css'
-import '@mantine/core/styles.css';
-import { MantineProvider, createTheme } from '@mantine/core';
-import { ColorSchemeScript } from '@mantine/core';
+import { MantineProvider, createTheme } from "@mantine/core";
+import { ColorSchemeScript } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+	? `https://${process.env.VERCEL_URL}`
+	: "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
-}
+	metadataBase: new URL(defaultUrl),
+	title: "Next.js and Supabase Starter Kit",
+	description: "The fastest way to build apps with Next.js and Supabase",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={GeistSans.className}>
-      <head><ColorSchemeScript /></head>
-      <body className="bg-background text-foreground">
-        <MantineProvider>
-          <main className="min-h-screen flex flex-col items-center">
-            {children}
-          </main>
-        </MantineProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" className={GeistSans.className}>
+			<head>
+				<ColorSchemeScript />
+			</head>
+			<body className="bg-background text-foreground">
+				<MantineProvider>
+					<main className="min-h-screen flex flex-col items-center">
+						{children}
+					</main>
+				</MantineProvider>
+			</body>
+		</html>
+	);
 }
