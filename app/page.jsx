1c829d607dc851796923b2725b5cdff3aed6acaa
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-08 22:03:53 -0800

    :fire: Wiped out random files

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,6 @@
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import Header from "@/components/Header";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
 import TestForm from "@/components/TestForm";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
-import AuthButton from "../components/AuthButton";
-import DeployButton from "../components/DeployButton";
 
 export default async function Index() {
-	const cookieStore = cookies();
-
-	const canInitSupabaseClient = () => {
-		// This function is just for the interactive tutorial.
-		// Feel free to remove it once you have Supabase connected.
-		try {
-			createClient(cookieStore);
-			return true;
-		} catch (e) {
-			return false;
-		}
-	};
-
-	const isSupabaseConnected = canInitSupabaseClient();
-
 	return (
 		<div>
 			<button className="btn btn-filled">hi this is a button</button>
