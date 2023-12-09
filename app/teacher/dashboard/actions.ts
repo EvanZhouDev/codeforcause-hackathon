70583742ca675a4266cd4d5c242c1013441e9fc0
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-09 13:57:30 -0800

    :recycle: Refactored dashboard

---
+++ app/teacher/dashboard/actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+ 
+export async function getClasses() {
+
+	const cookieStore = cookies();
+	const client = createClient(cookieStore);
+	const res = await client
+		.from("classes")
+		.select("name")
+		.eq("admin", (await client.auth.getUser()).data.user!.id);
+  return res;
+}
--- app/teacher/dashboard/components/StudentTable.jsx
+++ app/teacher/dashboard/components/StudentTable.jsx
@@ -1,6 +1,5 @@
 import Icon from "@/components/Icon";
-export default function StudentTable() {
-	let data = {};
+export default function StudentTable({ data }) {
 	return (
 		<div className="overflow-x-auto">
 			<table className="table mt-5">
@@ -14,28 +13,34 @@ export default function StudentTable() {
 					</tr>
 				</thead>
 				<tbody>
-					{/* row 1 */}
-					<tr>
-						<td>
-							<div className="flex items-center gap-3">
-								<div>
-									<div className="font-bold">Billy Bob Joe</div>
+					{data.map((student) => (
+						<tr>
+							<td>
+								<div className="flex items-center gap-3">
+									<div>
+										<div className="font-bold">
+											{student.name || "Not implemented"}
+										</div>
+									</div>
 								</div>
-							</div>
-						</td>
-						<td>bob.joe@gmail.com</td>
-						<td>10/13 classes</td>
-						<th>
-							<div className="flex">
-								<button className="ml-2 btn btn-secondary">
-									<Icon.Outlined className="w-4 h-4" name="InformationCircle" />
-								</button>
-								<button className="ml-2 btn btn-secondary">
-									<Icon.Outlined className="w-4 h-4" name="Trash" />
-								</button>
-							</div>
-						</th>
-					</tr>
+							</td>
+							<td>{student.email}</td>
+							<td>{student.attendence}</td>
+							<th>
+								<div className="flex">
+									<button className="ml-2 btn btn-secondary">
+										<Icon.Outlined
+											className="w-4 h-4"
+											name="InformationCircle"
+										/>
+									</button>
+									<button className="ml-2 btn btn-secondary">
+										<Icon.Outlined className="w-4 h-4" name="Trash" />
+									</button>
+								</div>
+							</th>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</div>
