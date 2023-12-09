a6f98eaf1b207eb4e2f493ae587e5996f3d1bfbb
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-08 16:18:09 -0800

    :fire: Removed Mantine

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-import { MantineProvider, createTheme } from "@mantine/core";
-import { ColorSchemeScript } from "@mantine/core";
-import "@mantine/core/styles.css";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
@@ -13,34 +10,6 @@ export const metadata = {
 	title: "Next.js and Supabase Starter Kit",
 	description: "The fastest way to build apps with Next.js and Supabase",
 };
-const theme = createTheme({
-	colors: {
-		evanGreen: [
-			"#f5f8f1",
-			"#eaefe4",
-			"#d2dec2",
-			"#b9cd9e",
-			"#a4be7f",
-			"#96b46b",
-			"#8eaf60",
-			"#7b9950",
-			"#6c8845",
-			"#5b7637"
-		],
-	},
-
-	// shadows: {
-	// 	md: '1px 1px 3px rgba(0, 0, 0, .25)',
-	// 	xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-	// },
-
-	// headings: {
-	// 	fontFamily: 'Roboto, sans-serif',
-	// 	sizes: {
-	// 		h1: { fontSize: rem(36) },
-	// 	},
-	// },
-});
 
 export default function RootLayout({
 	children,
@@ -49,15 +18,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className={GeistSans.className}>
-			<head>
-				<ColorSchemeScript />
-			</head>
 			<body className="bg-background text-foreground">
-				<MantineProvider theme={theme}>
-					<main className="min-h-screen flex flex-col items-center">
-						{children}
-					</main>
-				</MantineProvider>
+				<main className="min-h-screen flex flex-col items-center">
+					{children}
+				</main>
 			</body>
 		</html>
 	);
