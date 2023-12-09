0031145e0a0483145c42d9c3fd34eff91ebb11c3
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-09 13:58:33 -0800

    :art: Formatted

--- app/teacher/dashboard/actions.ts
+++ app/teacher/dashboard/actions.ts
@@ -1,16 +1,14 @@
-'use server'
+"use server";
 
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
- 
 export async function getClasses() {
-
 	const cookieStore = cookies();
 	const client = createClient(cookieStore);
 	const res = await client
 		.from("classes")
 		.select("name")
 		.eq("admin", (await client.auth.getUser()).data.user!.id);
-  return res;
+	return res;
 }
