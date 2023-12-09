6f75d60ae9d941cb340055117f02cdff58e186a1
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-08 16:52:30 -0800

    :sparkles: Evan's theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
+				// Some nice colors:
+				// Dark green: #496727
+				// Bright green: #C9EE9E
+				// Baby blue: #C5EAE7
+				// Weird magenta: #a7264b
+				// Bubblegum pink: #d46c91
 				background: "hsl(var(--background))",
 				foreground: "hsl(var(--foreground))",
 				btn: {
@@ -17,4 +23,19 @@ module.exports = {
 		},
 	},
 	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+		  {
+			materialYou: {
+			  primary: "#4f6630",
+			  "primary-content": "#ffffff",
+			  secondary: "#dfe6cf",
+			  "secondary-content": "#46483F",
+			  neutral: "#f5f4ec",
+			  "base-100": "#fefcf7",
+			}
+		  },
+		  "light"
+		]
+	  }
 };
